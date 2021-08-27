ea30fa535bc1ba0dfe0faf05ddca0f53245f01c4
aknajardhane <najardhaneaakanksha@gmail.com>
2021-08-27 18:51:24 +0530

    2nd commit

--- src/components/AddTask.js
+++ src/components/AddTask.js
@@ -1,58 +1,61 @@
-import { useState } from "react"
-const AddTask = ({onAdd}) => {
+import { useState } from "react";
+const AddTask = ({ onAdd }) => {
+  const [text, setText] = useState("");
+  const [day, setDay] = useState("");
+  const [reminder, setReminder] = useState(false);
 
-    const [text,setText] = useState('')
-    const [day,setDay] = useState('')
-    const [reminder,setReminder] = useState(false)
+  const onSubmit = (e) => {
+    e.preventDefault();
 
-    const onSubmit = (e) => {
-        e.preventDefault()
-
-        if(!text){
-            alert('Please add a task')
-            return
-        }
-
-        onAdd({text,day,reminder})
-
-        setText('')
-        setDay('')
-        setReminder(false)
+    if (!text) {
+      alert("Please add a task");
+      return;
     }
 
-    return (
-        <div>   <form className='add-form' onSubmit={onSubmit}>
-        <div    className="from-control">
-               <label>Task</label>
-       <input type='text' placeholder='Add Task'
-       value={text} 
-       onChange={(e) => setText(e.target.value)}
-       />
-       </div>
-      
-       <div className='from-control'>
-               <label>Day and Time </label>
-       <input type='text' placeholder='Add Day and Time'
-        value={day} 
-        onChange={(e) => setDay(e.target.value)} />
-       
-       </div>
-       <div className='from-control form-control-check'>
-               <label>Set Reminder</label>
-       <input type='checkbox'
-       checked={reminder }
-       placeholder='Set Reminder'
-       value={reminder} 
-        onChange={(e) => setReminder(e.currentTarget.checked)}
-       />
-       
-       </div>
-       <input type='submit' value='Save Task'
-       className='btn btn-block'/>
+    onAdd({ text, day, reminder });
 
-       </form> </div>
-      
-    )
-}
+    setText("");
+    setDay("");
+    setReminder(false);
+  };
 
-export default AddTask
+  return (
+    <div>
+      
+      <form className="add-form" onSubmit={onSubmit}>
+        <div className="from-control">
+          <label className="form-control-label">Task</label>
+          <input className="form-control-input"
+            type="text"
+            placeholder="Add Task"
+            value={text}
+            onChange={(e) => setText(e.target.value)}
+          />
+        </div>
+
+        <div className="from-control">
+          <label className="form-control-label" >Day and Time </label>
+          <input className="form-control-input"
+            type="text"
+            placeholder="Add Day and Time"
+            value={day}
+            onChange={(e) => setDay(e.target.value)}
+          />
+        </div>
+        <div className="from-control form-control-check">
+          <label className="form-control-label">Set Reminder</label>
+          <input className="form-control-input"
+            type="checkbox"
+            checked={reminder}
+            placeholder="Set Reminder"
+            value={reminder}
+            onChange={(e) => setReminder(e.currentTarget.checked)}
+          />
+        </div>
+        <input type="submit" value="Save Task" className="btn btn-block" />
+      </form>
+    </div>
+  );
+};
+
+export default AddTask;
